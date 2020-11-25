ac48c281ae5fd1c193527762d23471de1be36b2e
Мария <slinkywizard@mail.ru>
2020-11-25 23:32:03 +0300

    Верстка футера, меню-аккордеон в фильтре каталога

--- source/js/main.js
+++ source/js/main.js
@@ -65,3 +65,37 @@
   */
 
 })();
+
+// Переключение меню - аккордеона в блоке Частые вопросы
+var accordionItemTrigger = document.querySelectorAll(".accordion__item-trigger");
+var accordionItem = document.querySelectorAll(".accordion__item");
+var arrowItems = document.querySelectorAll(".accordion__item-svg");
+//var activeitem = document.querySelectorAll(".accordion__item--active");
+
+var activeArrow = document.querySelector(".accordion__item--active").querySelector(".accordion__item-svg");
+activeArrow.style.transform = "rotate(180deg)";
+
+/*
+activeItem.forEach(function (item) {
+  item.addEventListener("click")
+})
+*/
+accordionItemTrigger.forEach(function (item) {
+  item.addEventListener("click", () => {
+    arrowItems.forEach(function (item) {
+      item.style.transform = "none";
+    })
+
+    const parent = item.parentNode;
+    var arrowItem = item.querySelector(".accordion__item-svg");
+    arrowItem.style.transform = "rotate(360deg)";
+    if (parent.classList.contains("accordion__item--active")) {
+      parent.classList.remove("accordion__item--active");
+    } else {
+      accordionItem.forEach((child) => child.classList.remove("accordion__item--active"))
+
+      parent.classList.add("accordion__item--active");
+      arrowItem.style.transform = "rotate(180deg)";
+    }
+  })
+});
