b2fe6635ca0b8b7f80eca94c6819ef4dca943572
Мария <slinkywizard@mail.ru>
2020-11-26 23:27:51 +0300

    Верстка блоков: блог, галерея, частые вопросы

--- build/js/slider.js
+++ build/js/slider.js
@@ -59,20 +59,21 @@
     */
 
     // если совместить все, скорее всего будет так
-var swiper = new Swiper('.swiper-container', {
+var swiper = new Swiper(".swiper-container.swiper-container--newin", {
   slidesPerView: 4,
   spaceBetween: 30,
   slidesPerGroup: 3,
   loop: true,
   loopFillGroupWithBlank: true,
   pagination: {
-    el: '.swiper-pagination',
+    el: ".swiper-pagination",
     clickable: true,
   },
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
+  /*
   breakpoints: {
     320: {
       slidesPerView: 2,
@@ -82,9 +83,10 @@ var swiper = new Swiper('.swiper-container', {
       slidesPerView: 2,
       spaceBetween: 40,
     },
+    /*
     1024: {
       slidesPerView: 4,
       spaceBetween: 50,
     },
-  }
+  }*/
 });
