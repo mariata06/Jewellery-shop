b97cb7b1faff3dfed0be2d585053de4056cb93ce
Мария <slinkywizard@mail.ru>
2020-11-28 18:57:40 +0300

    Верстка слайдера на главной странице и в карточке товара

--- build/js/slider.js
+++ build/js/slider.js
@@ -40,25 +40,45 @@
     */
 
     // этот пролистывает по 3 слайда сразу
-    /*
-    var swiper = new Swiper('.swiper-container', {
-      slidesPerView: 3,
+
+var swiper = new Swiper('.swiper-container.swiper-container--newin', {
+  slidesPerView: 4,
+  spaceBetween: 30,
+  slidesPerGroup: 4,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + '</span>';
+    },
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    1024: {
+      slidesPerView: 4,
       spaceBetween: 30,
-      slidesPerGroup: 3,
-      loop: true,
-      loopFillGroupWithBlank: true,
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-    });
-    */
+      slidesPerGroup: 4,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+      slidesPerGroup: 2,
+    },
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+      slidesPerGroup: 2,
+    },
+  }
+});
 
     // если совместить все, скорее всего будет так
+    /*
 var swiper = new Swiper(".swiper-container.swiper-container--newin", {
   slidesPerView: 4,
   spaceBetween: 30,
@@ -88,5 +108,5 @@ var swiper = new Swiper(".swiper-container.swiper-container--newin", {
       slidesPerView: 4,
       spaceBetween: 50,
     },
-  }*/
-});
+  }
+});*/
