6d285706c47f533502c10ab46cbd1c9774de71b8
Мария <slinkywizard@mail.ru>
2020-11-28 23:51:05 +0300

    Верстка модального окна корзины

--- build/js/slider.js
+++ build/js/slider.js
@@ -1,46 +1,5 @@
 // Initialize Swiper
 
-/*
-    var swiper = new Swiper('.swiper-container', {
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-        renderBullet: function (index, className) {
-          return '<span class="' + className + '">' + (index + 1) + '</span>';
-        },
-      },
-    });
-*/
-
-    // или этот
-    /*
-    var swiper = new Swiper('.swiper-container', {
-      slidesPerView: 1,
-      spaceBetween: 10,
-      // init: false,
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
-      breakpoints: {
-        640: {
-          slidesPerView: 2,
-          spaceBetween: 20,
-        },
-        768: {
-          slidesPerView: 4,
-          spaceBetween: 40,
-        },
-        1024: {
-          slidesPerView: 5,
-          spaceBetween: 50,
-        },
-      }
-    });
-    */
-
-    // этот пролистывает по 3 слайда сразу
-
 var swiper = new Swiper('.swiper-container.swiper-container--newin', {
   slidesPerView: 4,
   spaceBetween: 30,
@@ -76,37 +35,3 @@ var swiper = new Swiper('.swiper-container.swiper-container--newin', {
     },
   }
 });
-
-    // если совместить все, скорее всего будет так
-    /*
-var swiper = new Swiper(".swiper-container.swiper-container--newin", {
-  slidesPerView: 4,
-  spaceBetween: 30,
-  slidesPerGroup: 3,
-  loop: true,
-  loopFillGroupWithBlank: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  /*
-  breakpoints: {
-    320: {
-      slidesPerView: 2,
-      spaceBetween: 20,
-    },
-    768: {
-      slidesPerView: 2,
-      spaceBetween: 40,
-    },
-    /*
-    1024: {
-      slidesPerView: 4,
-      spaceBetween: 50,
-    },
-  }
-});*/
